fe7fc71a61ff32f1e7390e4e6787fa2ceb1bccbc
Yeliang Shou <yeliang.shou@gmail.com>
2018-07-22 14:13:51 -0400

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', () => {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'jellyrulez@yousuck.com',
-        text: 'memememem'
-    });
-
 });
 
 socket.on('disconnect', () => {

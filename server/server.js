a1be86455c4900ea345c401a88e47c2a3c879958
Yeliang Shou <yeliang.shou@gmail.com>
2018-07-22 13:32:41 -0400

    Add newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'garbageboi@example.com',
+        text: 'I am no good',
+        createdAt: 12321
+    });
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message);
+    });
+    
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });

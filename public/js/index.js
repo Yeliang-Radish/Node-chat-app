26f84e00d1a1011e4ddd70f63999645b25371697
Yeliang Shou <yeliang.shou@gmail.com>
2018-07-22 22:52:46 -0400

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -10,28 +10,36 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>');
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        url: message.url,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-    $('#messages').append(li);
+    $('#messages').append(html);
 });
 
 $('#message-form').on('submit', (e) => {
     e.preventDefault();
 
     var messageTextbox = $('[name=message]');
-    
+
+    if (!messageTextbox.val()) {
+        return;
+    }
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextbox.val()
